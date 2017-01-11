25409dc7491e2befe829f68bff9a69f557cb514a
yingeddi2008 <ying.eddi2008@gmail.com>
2017-01-11 12:16:45 -0600

    2017.1.11 updated

--- R/best_cm.r
+++ R/best_cm.r
@@ -13,11 +13,11 @@
 #' best_cm(CV=cv,auc_out=auc_out)
 #'
 best_cm <- function(CV=cv, auc_out=auc_out){
-  bestrank <- auc_out$NumberOfFeature[which.max(auc_out$AUC_Prior_Area)]
+  bestrank <- auc_out$NumberOfFeature[which.max(auc_out$AUC_Area)]
   tmp <-lapply(cv,function(x) x[x$feature_rank==bestrank,])
   file_rank = data.frame(Reduce(function(...) merge(...,all=T),tmp))
 
-  pred <- as.numeric(as.numeric(as.vector(file_rank$Poster_Prio_Type1)) > as.numeric(as.vector(file_rank$Poster_Prio_Type2)))
+  pred <- as.numeric(as.numeric(as.vector(file_rank$Type1_Posterior_Prb)) > as.numeric(as.vector(file_rank$Type2_Postereior_Prb)))
   truth <- as.numeric(as.vector(file_rank$Type1Label))
 
   confusionMatrix(pred,truth)
