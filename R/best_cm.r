36440426586cd2e7b4de2c768d48d9cfe5c5b129
yingeddi2008 <ying.eddi2008@gmail.com>
2018-03-07 13:12:27 -0600

    change cv to CV

--- R/best_cm.r
+++ R/best_cm.r
@@ -21,7 +21,7 @@
 #'
 best_cm <- function(CV=cv, auc_out=auc_out){
   bestrank <- auc_out$NumberOfFeature[which.max(auc_out$AUC_Area)]
-  tmp <-lapply(cv,function(x) x[x$feature_rank==bestrank,])
+  tmp <-lapply(CV,function(x) x[x$feature_rank==bestrank,])
   file_rank = data.frame(Reduce(function(...) merge(...,all=T),tmp))
 
   pred <- as.numeric(as.numeric(as.vector(file_rank$Type1_Posterior_Prb)) > as.numeric(as.vector(file_rank$Type2_Postereior_Prb)))
